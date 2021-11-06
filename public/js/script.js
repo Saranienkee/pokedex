18b66fdb8dcc3d020450b44dc19cab374fbabfd1
Saranienkee <65567600+Saranienkee@users.noreply.github.com>
2021-11-06 01:19:41 -0600

    Add files via upload

---
+++ public/js/script.js
@@ -0,0 +1,56 @@
+function colores(){
+    if(type==="bug"){
+        colorArray.push("#99A816");
+    }
+    else if(type==="dark"){
+        colorArray.push("#2b2b2b");
+    }
+    else if(type==="dragon"){
+        colorArray.push("#715DDA");
+    }
+    else if(type==="electric"){
+        colorArray.push("#F9B816");
+    }
+    else if(type==="fairy"){
+        colorArray.push("#E79BE9");
+    }
+    else if(type==="fighting"){
+        colorArray.push("#6B2715");
+    }
+    else if(type==="fire"){
+        colorArray.push("#CD2804");
+    }
+    else if(type==="flying"){
+        colorArray.push("#8193E6");
+    }
+    else if(type==="ghost"){
+        colorArray.push("#52549D");
+    }
+    else if(type==="grass"){
+        colorArray.push("#6DBF31");
+    }
+    else if(type==="ground"){
+        colorArray.push("#CCAD58");
+    }
+    else if(type==="ice"){
+        colorArray.push("#74D5F4");
+    }
+    else if(type==="normal"){
+        colorArray.push("#C2BAB1");
+    }
+    else if(type==="poison"){
+        colorArray.push("#954997");
+    }
+    else if(type==="psychic"){
+        colorArray.push("#E34178");
+    }
+    else if(type==="rock"){
+        colorArray.push("#9E863A");
+    }
+    else if(type==="steel"){
+        colorArray.push("#A1A1B1");
+    }
+    else if(type==="water"){
+        colorArray.push("#1E7FDB");
+    }
+}
